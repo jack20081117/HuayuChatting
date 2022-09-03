8bf812072074b75c88104c92286cf30b18f46fd7
森岛船长 <1329913830@qq.com>
2022-09-03 13:23:49 +0800

    no message

--- core/extractor.py
+++ core/extractor.py
@@ -48,6 +48,11 @@ def generateUnknown(unknownqq):
         unknownqqs.append(unknownqq)
         return 'unknown'+str(unknown)
 
+def searchQQ(line:str)->str:
+    rline=line[::-1]
+    rqq=re.search(r'(?<=[)>])[^(<]+',rline).group()
+    return rqq[::-1]
+
 def extractHead(filepath:str)->list[str]:
     with open(filepath,'r',encoding='utf-8',errors='ignore') as reader:
         txt:str=reader.read()
@@ -71,7 +76,7 @@ def extract(filepath:str):
         month=re.search(r'20[\d-]{5}',time).group()
         date=re.search(r'20[\d-]{8}',time).group()#发言日期,YYYY-mm-dd
 
-        qq=re.search(r'(?<=[(<])[^)>]+',line).group()#发言者的qq号
+        qq=searchQQ(line)#发言者的qq号
         schoolID_raw=re.search(r'(?<=[\s】])(?:1[0-9]|2[0-5]|0[89])[1-8]\d\d',line)#发言者的学号
         #计算学号时要考虑:五位学号前两位应为08<=xx<=25,第三位由于班级个数取1-8,后两位理论上来说从01-99均有可能
         if schoolID_raw is not None:schoolID:str=schoolID_raw.group()#先确定发言者是否有学号(考虑到有机器人参与)
