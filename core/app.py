52f5fe1728831f096bfe4095c1cd714bc3d2c300
森岛船长 <1329913830@qq.com>
2023-01-28 13:42:58 +0800

    add form

--- core/app.py
+++ core/app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
 import os
 
 app=Flask(__name__)
@@ -7,27 +7,45 @@ app=Flask(__name__)
 def home():
     return render_template('home.html')
 
-@app.route('/month/chart/',methods=['GET','POST'])
+@app.route('/month/chart/',methods=['GET'])
 def monthchart():
     return render_template('monthchart.html')
+
+@app.route('/month/chart/',methods=['POST'])
+def monthchart_form():
+    schoolID=request.form['schoolID']
+    os.system('python monthchart_app.py --id %s'%schoolID)
+    return render_template('monthchart.html',schoolID=schoolID)
 
 @app.route('/month/chart/<schoolID>',methods=['GET','POST'])
 def monthchart_id(schoolID):
     os.system('python monthchart_app.py --id %s'%schoolID)
     return render_template('monthchart.html',schoolID=schoolID)
 
-@app.route('/week/chart/',methods=['GET','POST'])
+@app.route('/week/chart/',methods=['GET'])
 def weekchart():
     return render_template('weekchart.html')
+
+@app.route('/week/chart/',methods=['POST'])
+def weekchart_form():
+    schoolID=request.form['schoolID']
+    os.system('python weekchart_app.py --id %s'%schoolID)
+    return render_template('weekchart.html',schoolID=schoolID)
 
 @app.route('/week/chart/<schoolID>',methods=['GET','POST'])
 def weekchart_id(schoolID):
     os.system('python weekchart_app.py --id %s'%schoolID)
     return render_template('weekchart.html',schoolID=schoolID)
 
-@app.route('/day/chart/',methods=['GET','POST'])
+@app.route('/day/chart/',methods=['GET'])
 def daychart():
     return render_template('daychart.html')
+
+@app.route('/day/chart/',methods=['POST'])
+def daychart_form():
+    schoolID=request.form['schoolID']
+    os.system('python daychart_app.py --id %s'%schoolID)
+    return render_template('daychart.html',schoolID=schoolID)
 
 @app.route('/day/chart/<schoolID>',methods=['GET','POST'])
 def daychart_id(schoolID):
--- core/monthchart_app.py
+++ core/monthchart_app.py
@@ -33,6 +33,7 @@ def paint(schoolID):
     for key,value in data.items():
         xs.append(datetime.strptime(key,'%Y-%m').date())
         ys.append(value)
+    pyplot.figure(figsize=(10,5))
     pyplot.plot_date(xs,ys,linestyle='-',marker='.')
     pyplot.title(schoolID)
     pyplot.savefig("./static/img/monthchart.png")
